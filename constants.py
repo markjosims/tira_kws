8d12c134ba628f7889987823a02c920a28326a34
markjosims <mjsimmons@ucsd.edu>
2025-11-17 14:38:05 -0800

    add dataset paths

--- constants.py
+++ constants.py
@@ -1,5 +1,14 @@
 import torch
+import os
+
+# audio and model constants
 
 SAMPLE_RATE = 16_000
 DEVICE = torch.device(0 if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = 128
+
+# file paths
+
+DATA_DIR = os.environ.get("DATASETS", os.path.expanduser("~/datasets"))
+TIRA_ASR_PATH = os.path.join(DATA_DIR, "tira_asr")
+TIRA_DRZ_PATH = os.path.join(DATA_DIR, "tira_drz")
