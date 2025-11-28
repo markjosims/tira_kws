a8da22ceda5f7e3d2d8d44333ccfb38c8089fa5c
markjosims <mjsimmons@ucsd.edu>
2025-11-27 21:23:47 -0800

    use Path objects

--- constants.py
+++ constants.py
@@ -11,16 +11,16 @@ BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 256))
 # file paths
 
 ## dataset paths
-DATA_DIR = os.environ.get("DATASETS", os.path.expanduser("~/datasets"))
-TIRA_ASR_PATH = os.path.join(DATA_DIR, "tira_asr")
-TIRA_DRZ_PATH = os.path.join(DATA_DIR, "tira_drz")
+DATA_DIR = Path(os.environ.get("DATASETS", os.path.expanduser("~/datasets")))
+TIRA_ASR_PATH = DATA_DIR / "tira_asr"
+TIRA_DRZ_PATH = DATA_DIR / "tira_drz"
 
 ## local data paths
 CSV_PATH = Path("data/kws_predictions/most_predicted_word.csv")
 LABELS_DIR = Path("data/labels")
 KEYPHRASE_PATH = LABELS_DIR/"tira_keyphrases.txt"
 SIMILARITY_MATRIX_PATH = Path("data/similarity_matrix/similarity_matrix.pt")
-EMBEDDINGS_DIR = Path("data/embeddings")
+EMBEDDINGS_DIR = DATA_DIR/"tira_kws"/"embeddings"
 
 # keyword constants
 MAX_KEYWORD_STR = '$max'
