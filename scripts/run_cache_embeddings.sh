b449732484f55ec0eb3eea89045f617f1dfdf8cd
markjosims <mjsimmons@ucsd.edu>
2025-12-01 13:59:54 -0800

    change grid search params

--- scripts/run_cache_embeddings.sh
+++ scripts/run_cache_embeddings.sh
@@ -1,7 +1,18 @@
-for window_size in 0.05 0.1 0.2 0.5 1.0 2.0; do
-    echo "Caching embeddings with window size: $window_size"
-    for encoder_size in tiny base small; do
-        echo "  Using encoder size: $encoder_size"
+
+for encoder_size in base; do
+    echo "Caching embeddings encoder size: $encoder_size"
+    echo "  No windowing"
+    echo "    Caching TIRA ASR dataset..."
+    python -m scripts.cache_embeddings \
+        --dataset tira_asr \
+        --encoder_size $encoder_size
+    echo "    Caching TIRA DRZ dataset (z-scored to TIRA ASR)..."
+    python -m scripts.cache_embeddings \
+        --dataset tira_drz \
+        --encoder_size $encoder_size \
+        --whitened_from_dataset tira_asr
+    for window_size in 0.1 1.0 2.0; do
+        echo "  using window size: $window_size"
         echo "    Caching TIRA ASR dataset..."
         python -m scripts.cache_embeddings \
             --dataset tira_asr \
