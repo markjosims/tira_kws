5d32671cadceec6bb63194e6ea272462c8332cdb
markjosims <mjsimmons@ucsd.edu>
2025-12-01 13:55:21 -0800

    add constants for model names

--- constants.py
+++ constants.py
@@ -1,12 +1,23 @@
+from importlib.util import find_spec
 import torch
 import os
 from pathlib import Path
 
 # audio and model constants
 
+## hyperparameters
+
 SAMPLE_RATE = 16_000
 DEVICE = torch.device(0 if torch.cuda.is_available() else "cpu")
 BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 256))
+
+## model names
+
+CLAP_IPA_ENCODER_NAME = 'anyspeech/clap-ipa-{encoder_size}-speech'
+CLAP_IPA_TEXT_ENCODER_NAME = 'anyspeech/clap-ipa-{encoder_size}-phone'
+IPA_ALIGNER_ENCODER_NAME = 'anyspeech/ipa-align-{encoder_size}-speech'
+
+SPEECHBRAIN_LID_ENCODER_NAME = 'speechbrain/lang-id-voxlingua107-ecapa'
 
 # file paths
 
@@ -25,3 +36,5 @@ EMBEDDINGS_DIR = DATA_DIR/"tira_kws"/"embeddings"
 # keyword constants
 MAX_KEYWORD_STR = '$max'
 MEAN_KEYWORD_STR = '$mean'
+CLAP_IS_AVAILABLE = find_spec('clap') is not None
+SPEECHBRAIN_IS_AVAILABLE = find_spec('speechbrain') is not None
