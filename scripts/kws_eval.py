da596c42699ddd9038ce1b1cc22d3bad912ce6ee
markjosims <mjsimmons@ucsd.edu>
2025-12-12 10:05:22 -0800

    use torchmetrics classes

--- scripts/kws_eval.py
+++ scripts/kws_eval.py
@@ -1,6 +1,6 @@
 """
 Given a KWS list mapping keyphrases to positive and negative records,
-compute ROC AUC scores for keyword spotting using embeddings from
+compute ROC AUC scores for keyphrase spotting using embeddings from
 the given dataset and encoder.
 """
 
@@ -8,94 +8,130 @@ from argparse import ArgumentParser
 import json
 from constants import (
     KWS_PREDICTIONS, KEYPHRASE_LIST,
-    CALIBRATION_LIST, ENGLISH_CALIBRATION_LIST
+    CALIBRATION_LIST, ENGLISH_CALIBRATION_LIST,
+    DEVICE
 )
 from scripts.cache_embeddings import add_cache_embeddings_args, load_embeddings
 import pandas as pd
 import torch
-from torchmetrics.classification import BinaryAUROC, BinaryEER, BinaryROC
+from torchmetrics.classification import BinaryEER, BinaryROC
+from torchmetrics.retrieval import RetrievalMAP, RetrievalRecall, RetrievalAUROC
 from typing import *
 import wandb
 from dotenv import load_dotenv
 import os
+from tqdm import tqdm
 
 load_dotenv()
 EXPERIMENT_NAME_TEMPLATE = "tira_kws_auroc_ws_{encoder}_{window_size}"
 
-auroc = BinaryAUROC()
-eer = BinaryEER()
-roc = BinaryROC()
+# easy, medium and hard refer to different splits of Tira keyphrases
+# based on string similarity with the keyphrase query, with dissimilar
+# words in easy and similar words in hard
+# English indicates Tira/English LID, and is not binned by difficulty
+CASES = ['easy', 'medium', 'hard', 'english']
+METRIC_FUNCTS = {
+    'auroc': RetrievalAUROC,
+    'eer': BinaryEER,
+    'mAP': RetrievalMAP,
+    'recall': RetrievalRecall,
+}
+NON_RETRIEVAL_METRICS = ['eer']
+K_VALS = [1, 5, 10]
+
+METRICS = {}
+
+for case in CASES:
+    METRICS[case]={}
+    for metric in METRIC_FUNCTS:
+        METRICS[case][metric]=METRIC_FUNCTS[metric]().to(DEVICE)
+        # retrieval metrics calculated for various values of `k`
+        if metric not in NON_RETRIEVAL_METRICS:
+            for k in K_VALS:
+                METRICS[case][f"{metric}@{k}"]\
+                    =METRIC_FUNCTS[metric](top_k=k)\
+                    .to(DEVICE)
 
 def compute_metrics(
         positive_scores: torch.Tensor,
         negative_scores: torch.Tensor,
+        case: str,
+        index: int,
 ) -> Tuple[float, float]:
     """
-    Compute ROC AUC and EER given positive and negative scores.
+    Compute AUROC, EER, mAP and recall given positive and negative scores.
 
     Args:
         positive_scores: Tensor of shape (num_positive,)
         negative_scores: Tensor of shape (num_negative,)
+        case: str indicating case being evaluated
+        index: int indicating index of keyphrase being evaluated
     Returns:
-        Dict with 'auroc' and 'eer' scores
+        Dict with metric values
     """
+    batch_metrics = {}
+    if negative_scores.shape[0] == 0:
+        for metric in METRICS[case]:
+            batch_metrics[f"{case}/{metric}"] = torch.nan
+        return batch_metrics
+    
     labels = torch.cat([
         torch.ones_like(positive_scores),
         torch.zeros_like(negative_scores),
-    ])
+    ]).to(int)
     scores = torch.cat([positive_scores, negative_scores])
+    indices = torch.full_like(scores, index, dtype=int)
+    
+    for metric_name, metric_funct in METRICS[case].items():
+        if metric_name in NON_RETRIEVAL_METRICS:
+            metric_args = (scores, labels)
+        else:
+            metric_args = (scores, labels, indices)
+        batch_metrics[f"{case}/{metric_name}"] = metric_funct(*metric_args)
+    return batch_metrics
 
-    auroc_score = auroc(scores, labels).item()
-    eer_score = eer(scores, labels).item()
-    return auroc_score, eer_score
-
-def evaluate_keyword(
+def evaluate_keyphrase(
         tira_tira_scores: torch.Tensor,
         tira_eng_scores: torch.Tensor,
-        keyword_object: Dict[str, Any],
-        eng_idcs: Optional[Set[int]] = None,
+        keyphrase_object: Dict[str, Any],
 ) -> Dict[str, float]:
     """
-    Get ROC AUC and EER for a single keyword, divided into easy, medium and hard
+    Get ROC AUC and EER for a single keyphrase, divided into easy, medium and hard
     for Tira>Tira KWS and a single score for Tira>English KWS.
 
     Args:
         tira_tira_scores: Tensor of shape (num_tira_records, num_tira_records)
         tira_eng_scores: Tensor of shape (num_tira_records, num_eng_records)
-        keyword_object: Dict with keys 'keyphrase', 'keyphrase_idx', 'record_idcs',
+        keyphrase_object: Dict with keys 'keyphrase', 'keyphrase_idx', 'record_idcs',
             'easy', 'medium', 'hard'
         eng_idcs: Optional set of English keyphrase indices to use for
             negative examples. If None, use all English keyphrases.
     Returns:
-        Dict with AUROC and EER scores
+        List of dicts with keyphrases and metric values
     """
-    positive_idcs = keyword_object['record_idcs']
-    easy_idcs = keyword_object['easy']
-    medium_idcs = keyword_object['medium']
-    hard_idcs = keyword_object['hard']
-    if eng_idcs is None:
-        eng_idcs = torch.arange(tira_eng_scores.shape[1])
+    positive_idcs = keyphrase_object['record_idcs']
+    keyphrase_idx = keyphrase_object['keyphrase_idx']
     results = []
     for query_idx in positive_idcs:
         batch_positive_idcs = torch.tensor(positive_idcs)
         batch_positive_idcs = batch_positive_idcs[batch_positive_idcs != query_idx]
-        batch_result = dict(keyphrase=keyword_object['keyphrase'], query_idx=query_idx)
+        batch_result = dict(keyphrase=keyphrase_object['keyphrase'], query_idx=query_idx)
 
-        # Tira > Tira
-        for negative_idcs, difficulty in [
-            (easy_idcs, 'easy'), (medium_idcs, 'medium'), (hard_idcs, 'hard')
-        ]:
+        for case in CASES:
             positive_scores = tira_tira_scores[query_idx, batch_positive_idcs]
-            negative_scores = tira_tira_scores[query_idx, negative_idcs]
-            auroc_score, eer_score = compute_metrics(positive_scores, negative_scores)
-            batch_result[f'{difficulty}/auroc'] = auroc_score
-            batch_result[f'{difficulty}/eer'] = eer_score
-        # Tira > English
-        positive_scores = tira_tira_scores[query_idx, batch_positive_idcs]
-        negative_scores = tira_eng_scores[query_idx, eng_idcs]
-        auroc_score, eer_score = compute_metrics(positive_scores, negative_scores)
-        batch_result[f'eng/auroc'] = auroc_score
-        batch_result[f'eng/eer'] = eer_score
+            
+            if case == 'english':
+                negative_scores = tira_eng_scores[query_idx]
+            else:
+                negative_idcs = keyphrase_object[case]
+                negative_scores = tira_tira_scores[query_idx, negative_idcs]
+            batch_metrics = compute_metrics(
+                positive_scores,
+                negative_scores,
+                case,
+                keyphrase_idx,
+            )
+            batch_result.update(**batch_metrics)
         results.append(batch_result)
     return results
 
@@ -133,21 +169,24 @@ def compute_roc_auc(args, run=None) -> pd.DataFrame:
     tira_embeds = tira_embed_dict['embeddings']
     eng_embeds = eng_embed_dict['embeddings']
 
-    tira_tira_similarity = get_similarity_scores(tira_embeds, tira_embeds)
-    tira_eng_similarity = get_similarity_scores(tira_embeds, eng_embeds)
-
     kws_list = load_kws_list(args)
     eng_idcs = load_eng_list(args)
 
+    if eng_idcs is not None:
+        eng_embeds = eng_embeds[eng_idcs]
+
+    tira_tira_similarity = get_similarity_scores(tira_embeds, tira_embeds)
+    tira_eng_similarity = get_similarity_scores(tira_embeds, eng_embeds)
+
     results = []
-    for keyword_object in kws_list:
-        keyword_results = evaluate_keyword(
+    for keyphrase_object in tqdm(kws_list, desc='Evaluating keyphrases...'):
+        keyphrase_results = evaluate_keyphrase(
             tira_tira_scores=tira_tira_similarity,
             tira_eng_scores=tira_eng_similarity,
-            keyword_object=keyword_object,
-            eng_idcs=eng_idcs,
+            keyphrase_object=keyphrase_object,
         )
-        results.extend(keyword_results)
+        results.extend(keyphrase_results)
+
     df = pd.DataFrame(results)
     return df
 
@@ -177,14 +216,22 @@ def get_list_path(args):
 
 def main():
     args = parse_args()
-    with wandb.init(project=os.environ.get('WANDB_PROJECT', None)) as run:
+    with wandb.init(
+        project=os.environ.get('WANDB_PROJECT', None),
+        entity=os.environ.get('WANDB_ENTITY', None),
+    ) as run:
         experiment_name = get_experiment_name(args)
         run.name = experiment_name
         run.config.update(vars(args))
 
         df = compute_roc_auc(args, run=run)
 
-    run.log({"roc_auc_results": wandb.Table(dataframe=df)})
+        run.log({"roc_auc_results": wandb.Table(dataframe=df)})
+        for case, case_metrics in METRICS.items():
+            print(f"Computing overall metrics for case {case}...")
+            for metric_name, metric_obj in case_metrics.items():
+                run.summary[f"{case}/{metric_name}"] = metric_obj.compute()
+                
 
     output_file = KWS_PREDICTIONS / f"{experiment_name}.csv"
     df.to_csv(output_file, index=False)
