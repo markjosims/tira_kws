83c9b905173d05fabba17bf11e6514104470911c
Mark J Simmons <mjsimmons@ucsd.edu>
2026-02-10 13:01:30 -0800

    environment config w/ k2 and icefall

---
+++ setup_environ.sh
@@ -0,0 +1,16 @@
+current_dir=$(pwd)
+parent_dir=$(dirname "$current_dir")
+uv venv --python=3.10
+source .venv/bin/activate
+uv pip install -r requirements.txt
+uv pip install k2==1.24.4.dev20251118+cuda12.9.torch2.9.1 -f https://k2-fsa.github.io/k2/cuda.html
+uv pip install git+https://github.com/lhotse-speech/lhotse
+cd $parent_dir
+if [ ! -d "icefall" ]; then
+  git clone https://github.com/k2-fsa/icefall
+fi
+cd icefall
+uv pip install -r requirements.txt
+uv pip install -e .
+cd $current_dir
+echo "export PYTHONPATH=$parent_dir/icefall:$PYTHONPATH" >> .venv/bin/activate
